e84c6331e5c3f6f49c69b11e58ee88a9024a184f
mikel <mikimus113@gmail.com>
2017-07-17 05:43:28 +0000

    Fin de l'api

--- src/net/moozisms/api/client/MoozismsApiClient.java
+++ src/net/moozisms/api/client/MoozismsApiClient.java
@@ -5,6 +5,10 @@
  */
 package net.moozisms.api.client;
 
+import java.util.List;
+import java.util.Map;
+
+
 /**
  *
  * @author anonymousghost
@@ -12,16 +16,7 @@ package net.moozisms.api.client;
 public interface MoozismsApiClient {
         
     
-    /**
-     * 
-     * @param api_key Your api_key generate from Moozisms plateform, example : vLaR6iXDoLrvSPzy
-     * @param api_secret Your api_secret generate from Moozisms plateform, example : fbb2c6a0-5533-11e7-806e-cfdc8z11ccao
-     * @param to Phonenumber of the recipient with code without 00 or +, example : 22890147895(00228 or +228 and 90147895, phonenumber from TOGO),13483158714(001 or +1 and 3483158714, phonenumber from USA)
-     * @param from Sender ID less than 11 characters, example : Administration
-     * @param text Your messages without the special characters
-     * @param datatype json or xml
-     */
-     public void send(String api_key,String api_secret,String to,String from,String text,String datatype);
+    
      
      
      /**
@@ -31,8 +26,19 @@ public interface MoozismsApiClient {
       * @param to Phonenumber of the recipient with code without 00 or +, example : 22890147895(00228 or +228 and 90147895, phonenumber from TOGO),13483158714(001 or +1 and 3483158714, phonenumber from USA)
       * @param from Sender ID less than 11 characters, example : Administration
       * @param text Your messages without the special characters
-      * @param datatype json or xml
       * @return true if message is sent and false if message is not sent
       */
-     public boolean sendWithAcknowledgmentOfReceipt(String api_key,String api_secret,String to,String from,String text,String datatype);
+     public boolean sendSimple(String api_key,String api_secret,String to,String from,String text);
+     
+     
+     /**
+      * 
+      * @param api_key Your api_key generate from Moozisms plateform, example : vLaR6iXDoLrvSPzy
+      * @param api_secret Your api_secret generate from Moozisms plateform, example : fbb2c6a0-5533-11e7-806e-cfdc8z11ccao
+      * @param toList List of Phonenumber with code without 00 or +, example : 22890147895(00228 or +228 and 90147895, phonenumber from TOGO),13483158714(001 or +1 and 3483158714, phonenumber from USA)
+      * @param from Sender ID less than 11 characters, example : Administration
+      * @param text Your messages without the special characters
+      * @return a Map wich contains phonenumber with acknowledgement of recipient or null if messages are not sent
+      */
+     public Map<String,Boolean> sendGroup(String api_key, String api_secret, List<String> toList, String from, String text);
 }
