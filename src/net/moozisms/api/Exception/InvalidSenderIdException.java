284cf282ea468f475bed01cda216489016702135
Mikeezy Kazama <mikimus113@gmail.com>
2017-08-07 13:22:14 +0000

    Push pour remplacer le dernier jar de moozisms

---
+++ src/net/moozisms/api/Exception/InvalidSenderIdException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.moozisms.api.Exception;
+
+/**
+ *
+ * @author anonymousghost
+ */
+public class InvalidSenderIdException extends Exception {
+
+    /**
+     * Creates a new instance of <code>InvalidSenderIdException</code> without
+     * detail message.
+     */
+    public InvalidSenderIdException() {
+    }
+
+    /**
+     * Constructs an instance of <code>InvalidSenderIdException</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public InvalidSenderIdException(String msg) {
+        super(msg);
+    }
+}
